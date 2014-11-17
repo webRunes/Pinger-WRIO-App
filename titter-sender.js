edddc74bf7010952adb76fcd5a28f178c05bac47
bhushano7 <bhushan2250@gmail.com>
2014-11-18 00:23:39 +0530

    update nconf

--- titter-sender.js
+++ titter-sender.js
@@ -1,25 +1,24 @@
 var Twitter = require('node-twitter');
-
-var titterPicture = require('./titter-picture')
-var config = require('./config.json')
+var titterPicture = require('./titter-picture');
+var nconf = require("./wrio_nconf.js").init();
 
 var twitter = new Twitter.RestClient(
-  config.consumer_key,
-  config.consumer_secret,
-  config.token,
-  config.token_secret
+	nconf.get('api:twitter:consumerKey'),
+	nconf.get('api:twitter:consumerSecret'),
+	nconf.get('api:twitter:token'),
+	nconf.get('api:twitter:tokenSecret')
 )
 
 var titterSender = {};
 
-titterSender.comment = function(imgSrc, callback) {
-  twitter.statusesUpdateWithMedia(
-    { 
-    'status': "",
-    'media[]': imgSrc 
-    }, 
-    callback
-  );
+titterSender.comment = function (imgSrc, callback) {
+	twitter.statusesUpdateWithMedia(
+		{
+			'status': "",
+			'media[]': imgSrc
+		},
+		callback
+	);
 }
 
 module.exports = titterSender;
