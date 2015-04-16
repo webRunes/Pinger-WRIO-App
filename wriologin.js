166ee698f4b64f8d12b5e31af13a3da72bb6b117
michbil <denso.ffff@gmail.com>
2015-04-17 02:18:56 +0300

    change

--- wriologin.js
+++ wriologin.js
@@ -14,13 +14,36 @@ MYSQL_PASSWORD = nconf.get("db:password");
 MYSQL_DB = nconf.get("db:dbname");
 DOMAIN= nconf.get("db:workdomain");
 
-var connection = mysql.createConnection({
-    host     : MYSQL_HOST,
-    user     : MYSQL_USER,
-    password : MYSQL_PASSWORD
-});
+function handleDisconnect() {
+    connection = mysql.createConnection({
+        host     : MYSQL_HOST,
+        user     : MYSQL_USER,
+        password : MYSQL_PASSWORD
+    });
 
-connection.query('USE '+MYSQL_DB);
+
+
+    connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        } else {
+            console.log("Connecting to db...")
+            connection.query('USE '+MYSQL_DB);
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 // used to deserialize the user
 function deserialize(id, done) {
