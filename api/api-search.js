c29bcb7f19915b9c02495a328a7fe736f13486bd
Aleksey Krasikov <alekseykrasikov.hk@gmail.com>
2015-05-27 14:14:45 +0000

    Add return to the error handling

--- api/api-search.js
+++ api/api-search.js
@@ -4,7 +4,9 @@ var Twit = require('twit');
 router.post('/search', function(request, response) {
     var T = new Twit(request.body.twitter_creds);
     T.get('search/tweets', request.body.query, function(err, item) {
-        if (err) console.log('Problems with Twitter');
+        if (err) {
+            return console.log(err.message);
+        }
         
     	console.log('Got ' + item.statuses.length + ' statuses');
     	if (item.statuses.length > 0) {
