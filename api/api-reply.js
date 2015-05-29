9b41e64562864694ae52b16be053063c212870c6
Aleksey Krasikov <alekseykrasikov.hk@gmail.com>
2015-05-29 14:59:28 +0000

    Change replyAll method to reply images in addition to status

--- api/api-reply.js
+++ api/api-reply.js
@@ -1,27 +1,45 @@
 var router = require('express').Router();
 var Twit = require('twit');
+var titterPicture = require('../titter-picture');
+var titterSender = require('../titter-sender');
+var fs = require("fs");
+var Twitter = require('node-twitter');
+
 
 router.post('/replyAll', function(request, response){
-    var T = new Twit(request.body.twitterCreds);
+    var creds = request.body.twitterCreds
+    var twitterRestClient = new Twitter.RestClient(
+        creds.consumer_key,
+        creds.consumer_secret,
+        creds.access_token,
+        creds.access_token_secret
+    );
     var statuses = request.body.statuses;
     
     statuses.forEach(function(status) {
-        var replyText = '@' + status.user.screen_name + ' ' + request.body.message
-        console.log('Replying to @' + status.user.screen_name + ': ' + replyText);
-        var query = {
-            status: replyText,
-    		screen_name: status.user.screen_name,
-    		in_reply_to_status_id: status.id_str
-        }
+        var replyText = '@' + status.user.screen_name + ' ' + request.body.message;
         
-        T.post('statuses/update', query, function(err, data) {
-    		if (err) {
-    			return response.status(402).send(err.message);
-    		}
-    		
-    		console.log('Successfully replied to @' + status.user.screen_name);
-    		response.status(200).end();
-    	});    
+        titterPicture.drawComment(replyText, function(err, filename) {
+            if (err) {
+                return console.log('Draw comment error:', err.message);
+            }
+            
+            var query = {
+                'status': replyText,
+                'media[]': filename,
+    		    screen_name: status.user.screen_name,
+    		    in_reply_to_status_id: status.id_str
+            }
+            
+            twitterRestClient.statusesUpdateWithMedia(query, function(err, result) {
+                    if (err) {
+                        return response.status(err.code).send(err.message);
+                    }
+            
+                    response.status(200).end();
+                }
+            );
+		});
     });
 });
 
