be72b34cba29ad8c1980288e9d5cdc7f2170df21
michbil <denso.ffff@gmail.com>
2015-06-02 22:15:20 +0300

    change wrio header

--- wrio_app.js
+++ wrio_app.js
@@ -6,7 +6,7 @@ exports.init = function (express) {
 	// Add headers
 	app.use(function (request, response, next) {
 		//console.log(request);
-		response.setHeader('Access-Control-Allow-Origin', 'http://webrunes.com');
+		response.setHeader('Access-Control-Allow-Origin', 'http://wrioos.com');
 		response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 		response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 		response.setHeader('Access-Control-Allow-Credentials', true);
