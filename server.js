d0ec6f6a6fefb62feab091f16ac56bd4585c453d
michbil <denso.ffff@gmail.com>
2015-08-23 19:20:12 +0300

    Begin mongo transition refactoring

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+'use strict';
 var nconf = require("./wrio_nconf.js")
 	.init();
+
+var fs = require('fs');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var wrioLogin;
+var titterPicture = require('./titter-picture');
+var titterSender = require('./titter-sender');
+var DOMAIN = nconf.get('db:workdomain');
+
 var express = require('express');
 var app = require("./wrio_app.js")
 	.init(express);
@@ -13,135 +23,124 @@ var server = require('http')
 		var url = 'mongodb://' + nconf.get('mongo:user') + ':' + nconf.get('mongo:password') + '@' + nconf.get('mongo:host') + '/' + nconf.get('mongo:dbname');
 		MongoClient.connect(url, function(err, db) {
 			if (err) {
-				console.log("Error coonect to database: " + err);
+				console.log("Error conecting to mongo database: " + err);
 			} else {
 				app.custom.db = db;
 				//(require('./chess/route.js'))({
 				//	app: app
 				//});
-				console.log("Connected correctly to db server");
+				console.log("Connected correctly to mongodb server");
+				wrioLogin= require('./wriologin')(db);
+				server_setup();
 			}
 		});
 	});
 
-var fs = require('fs');
 
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
+function server_setup() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
 
-var wrioLogin = require('./wriologin');
+//var SessionStore = require('express-mysql-session');
+	var SessionStore = require('connect-mongo')(session);
+	var cookie_secret = nconf.get("server:cookiesecret");
+	app.use(cookieParser(cookie_secret));
+	var sessionStore = new SessionStore({db: app.custom.db});
+	app.use(session(
+		{
 
-var titterPicture = require('./titter-picture');
-var titterSender = require('./titter-sender');
+			secret: cookie_secret,
+			saveUninitialized: true,
+			store: sessionStore,
+			resave: true,
+			cookie: {
+				secure: false,
+				domain: DOMAIN,
+				maxAge: 1000 * 60 * 60 * 24 * 30
+			},
+			key: 'sid'
+		}
+	));
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+	var p3p = require('p3p');
+	app.use(p3p(p3p.recommended));
 
-var SessionStore = require('express-mysql-session');
-var cookie_secret = nconf.get("server:cookiesecret");
-app.use(cookieParser(cookie_secret));
-var session_options = {
-		host: MYSQL_HOST,
-		port: 3306,
-		user: MYSQL_USER,
-		password: MYSQL_PASSWORD,
-		database: MYSQL_DB
-}
-var sessionStore = new SessionStore(session_options);
-app.use(session(
-	{
+	var argv = require('minimist')(process.argv.slice(2));
+	if (argv.testjsx == "true") {
+		console.log("\nEntering jsx widget test mode, use /test.html to check widget operation\n");
+		app.use(express.static(__dirname + '/widget'));
+		app.use(express.static(__dirname + '/test'));
 
-		secret: cookie_secret,
-		saveUninitialized: true,
-		store: sessionStore,
-		resave: true,
-		cookie: {
-			secure:false,
-			domain:DOMAIN,
-			maxAge: 1000 * 60 * 60 * 24 * 30
-		},
-		key: 'sid'
 	}
-));
 
-var p3p = require('p3p');
-app.use(p3p(p3p.recommended));
-
-var argv = require('minimist')(process.argv.slice(2));
-if (argv.testjsx == "true") {
-	console.log("\nEntering jsx widget test mode, use /test.html to check widget operation\n");
-	app.use(express.static(__dirname + '/widget'));
-	app.use(express.static(__dirname + '/test'));
-
-}
-
-app.get('/', function(request, response) {
-	console.log(request.sessionID);
-	wrioLogin.loginWithSessionId(request.sessionID, function(err, res) {
-		if (err) {
-			console.log("User not found:",err);
-			response.render('index.ejs', {
-				"error": "Not logged in",
-				"user": undefined
-			});
-		} else {
-			response.render('index.ejs', {
-				"user": res
-			});
-			console.log("User found " + res);
-		}
-	})
-});
-
-
-app.get('/logoff', function(request, response) {
-	console.log("Logoff called");
-	response.clearCookie('sid', {
-		'path': '/',
-		'domain': DOMAIN
-	});
-	response.redirect('/');
-
-});
-
-
-app.get('/callback', function(request, response) {
-	console.log("Our callback called");
-	response.render('callback', {});
-});
-
-app.post('/sendComment', function(request, response) {
-
-	var text = request.body.text;
-	var title = request.body.title;
-	var message = request.body.comment;
-	//var ssid = request.body.ssid;
-	var ssid = request.sessionID;
-	console.log("Sending comment " + message + " with ssid " + ssid);
-
-	wrioLogin.getTwitterCredentials(ssid, function(err, cred) {
-		if (err) {
-			console.log("Twitter auth failed");
-			//response.send('Failed');
-			response.status(401);
-			response.send('Failed: ' + err);
-			return;
-		} else {
-			console.log("got keys", cred);
-			titterPicture.drawComment(text, function(error, filename) {
-				titterSender.comment(cred, title + '\n' + message + ' Donate 0 WRG', filename, function done(err, res) {
-					if (err) {
-						response.status(400);
-						response.send(err);
-					} else {
-						response.send('Done');
-					}
+	app.get('/', function (request, response) {
+		console.log(request.sessionID);
+		wrioLogin.loginWithSessionId(request.sessionID, function (err, res) {
+			if (err) {
+				console.log("User not found:", err);
+				response.render('index.ejs', {
+					"error": "Not logged in",
+					"user": undefined
 				});
-
-			});
-		}
+			} else {
+				response.render('index.ejs', {
+					"user": res
+				});
+				console.log("User found " + res);
+			}
+		})
 	});
 
-});
 
-console.log("Application Started!");
+	app.get('/logoff', function (request, response) {
+		console.log("Logoff called");
+		response.clearCookie('sid', {
+			'path': '/',
+			'domain': DOMAIN
+		});
+		response.redirect('/');
+
+	});
+
+
+	app.get('/callback', function (request, response) {
+		console.log("Our callback called");
+		response.render('callback', {});
+	});
+
+	app.post('/sendComment', function (request, response) {
+
+		var text = request.body.text;
+		var title = request.body.title;
+		var message = request.body.comment;
+		//var ssid = request.body.ssid;
+		var ssid = request.sessionID;
+		console.log("Sending comment " + message + " with ssid " + ssid);
+
+		wrioLogin.getTwitterCredentials(ssid, function (err, cred) {
+			if (err) {
+				console.log("Twitter auth failed");
+				//response.send('Failed');
+				response.status(401);
+				response.send('Failed: ' + err);
+				return;
+			} else {
+				console.log("got keys", cred);
+				titterPicture.drawComment(text, function (error, filename) {
+					titterSender.comment(cred, title + '\n' + message + ' Donate 0 WRG', filename, function done(err, res) {
+						if (err) {
+							response.status(400);
+							response.send(err);
+						} else {
+							response.send('Done');
+						}
+					});
+
+				});
+			}
+		});
+
+	});
+
+	console.log("Titter server config finished");
+};
--- wriologin.js
+++ wriologin.js
@@ -1,119 +1,88 @@
+"use strict";
 /**
  * Created by mich.bil on 16.04.15.
  */
 
 var nconf = require("./wrio_nconf.js")
 	.init();
-var mysql = require('mysql');
 
-MYSQL_HOST = nconf.get("db:host");
-MYSQL_USER = nconf.get("db:user");
-MYSQL_PASSWORD = nconf.get("db:password");
-MYSQL_DB = nconf.get("db:dbname");
-DOMAIN = nconf.get("db:workdomain");
-
-function handleDisconnect() {
-	connection = mysql.createConnection({
-		host: MYSQL_HOST,
-		user: MYSQL_USER,
-		password: MYSQL_PASSWORD
-	});
-
-
-
-	connection.connect(function(err) { // The server is either down
-		if (err) { // or restarting (takes a while sometimes).
-			console.log('error when connecting to db:', err);
-			setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-		} else {
-			console.log("Connecting to db...")
-			connection.query('USE ' + MYSQL_DB);
-		} // to avoid a hot loop, and to allow our node script to
-	}); // process asynchronous requests in the meantime.
-	// If you're also serving http, display a 503 error.
-	connection.on('error', function(err) {
-		console.log('db error', err);
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-			handleDisconnect(); // lost due to either server restart, or a
-		} else { // connnection idle timeout (the wait_timeout
-			throw err; // server variable configures this)
-		}
-	});
-}
-
-handleDisconnect();
-
-// used to deserialize the user
-function deserialize(id, done) {
-	console.log("Deserializing user by id=" + id)
-	connection.query("select * from `webRunes_Users` where userID =" + id, function(err, rows) {
-		if (err) {
-			console.log("User not found", err);
-			done(err);
-			return;
-		}
-
-		done(err, rows[0]);
-	});
-};
-
-function loginWithSessionId(ssid, done) {
-	match = ssid.match(/^[-A-Za-z0-9+/=_]+$/m);
-	if (!match) {
-		console.log("Wrong ssid");
-		done("Error");
-		return
-	}
-	q = "select * from sessions where session_id =\"" + ssid + "\"";
-	connection.query(q, function(err, rows) {
-		if (err) {
-			console.log("User not found", err);
-			done(err);
-			return;
-		}
-		if (rows[0] == undefined) {
-			done("Session not found");
-			return;
-		}
-		console.log("Session deserialized " + ssid, rows[0]);
-		data = JSON.parse(rows[0].data);
-
-		if (data.passport) {
-			user = data.passport.user;
-		} else {
-			user = undefined;
-		}
-
-
-
-		if (user != undefined) {
-			deserialize(user, done);
-		} else {
-			done("Wrong cookie")
-		}
-
-		//done(err, rows[0]);
-	});
-}
-
-function getTwitterCredentials(sessionId, done) {
-
-	loginWithSessionId(sessionId, function callback(err, res) {
-		if (err) {
-			console.log("Error executing request");
-			done(err);
-		} else {
-			if (res.token && res.tokenSecret) {
-				done(null, {
-					"token": res.token,
-					"tokenSecret": res.tokenSecret
-				})
-			} else {
-				done("No login with twitter");
+var $ = function (db) {
+	var webrunesUsers = db.collection('webRunes_Users');
+	var sessions = db.collection('sessions');
+	// used to deserialize the user
+	function deserialize(id, done) {
+		console.log("Deserializing user by id=" + id);
+		webrunesUsers.findOne({userID:id},function (err,user) {
+			if (err) {
+				console.log("User not found", err);
+				done(err);
+				return;
 			}
-		}
-	});
-}
 
-module.exports.loginWithSessionId = loginWithSessionId;
-module.exports.getTwitterCredentials = getTwitterCredentials;
+			done(err, user);
+		});
+	};
+
+	function loginWithSessionId(ssid, done) {
+		var match = ssid.match(/^[-A-Za-z0-9+/=_]+$/m);
+		if (!match) {
+			console.log("Wrong ssid");
+			done("Error");
+			return
+		}
+		var q = "select * from sessions where session_id =\"" + ssid + "\"";
+		sessions.findOne({_id: ssid},function(err, session) {
+			if (err) {
+				console.log("User not found", err);
+				done(err);
+				return;
+			}
+
+			console.log("Session deserialized " + ssid, session);
+			var data = session.session;
+
+			if (data.passport) {
+				var user = data.passport.user;
+			} else {
+				user = undefined;
+			}
+
+
+
+			if (user != undefined) {
+				deserialize(user, done);
+			} else {
+				done("Wrong cookie")
+			}
+
+			//done(err, rows[0]);
+		});
+	}
+
+	function getTwitterCredentials(sessionId, done) {
+
+		loginWithSessionId(sessionId, function callback(err, res) {
+			if (err) {
+				console.log("Error executing request");
+				done(err);
+			} else {
+				if (res.token && res.tokenSecret) {
+					done(null, {
+						"token": res.token,
+						"tokenSecret": res.tokenSecret
+					})
+				} else {
+					done("No login with twitter");
+				}
+			}
+		});
+	}
+
+	return {
+		loginWithSessionId: loginWithSessionId,
+		getTwitterCredentials: getTwitterCredentials
+	}
+
+};
+module.exports = $;
+
