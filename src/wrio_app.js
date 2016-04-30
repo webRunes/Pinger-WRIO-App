e6dc874e25fd1cc02a314b84787d1a8f291a20eb
Michael Bilenko <denso.ffff@gmail.com>
2016-04-30 23:47:52 +0300

    Fix CORS headers during docker debug

--- src/wrio_app.js
+++ src/wrio_app.js
@@ -1,3 +1,6 @@
+import nconf from 'nconf';
+import logger from 'winston';
+
 var exports = module.exports = {};
 
 exports.init = function(express) {
@@ -6,7 +9,21 @@ exports.init = function(express) {
     // Add headers
     app.use(function(request, response, next) {
         //console.log(request);
-        response.setHeader('Access-Control-Allow-Origin', 'http://wrioos.com');
+        var host = request.get('origin');
+        if (host == undefined) host = "";
+
+        var domain = nconf.get("db:workdomain");
+
+        domain = domain.replace(/\./g,'\\.')+'$';
+        logger.log('silly',domain);
+
+        if (host.match(new RegExp(domain,'m'))) {
+            response.setHeader('Access-Control-Allow-Origin', host);
+            logger.log("debug","Allowing CORS for webrunes domains");
+        } else {
+            logger.log("debug",'host not match');
+        }
+
         response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         response.setHeader('Access-Control-Allow-Credentials', true);
